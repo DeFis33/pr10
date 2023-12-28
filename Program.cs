c7fd836cf137f6f976879ef3e485849d8a419370
DeFis33 <61848806+DeFis33@users.noreply.github.com>
2023-12-28 20:35:02 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -19,10 +19,11 @@ namespace pr10
             {
                 try
                 {
-                    int M = 3, N = 5; 
-                    Random rnd = new Random(); 
-                    int[,] Mas = new int[M, N]; 
+                    int M = 3, N = 5;
+                    Random rnd = new Random();
+                    int[,] Mas = new int[M, N];
                     int product = 1;
+                    bool NotEmpty = false;
 
                     Console.WriteLine("\nХотите ли бы вы сами ввести элементы матрицы? (Да/Нет).\nЛибо введите команду (Выход), которая завершит работу программы:");
                     string a = Console.ReadLine();
@@ -32,7 +33,7 @@ namespace pr10
                         Console.WriteLine("Программа завершена.\nДо свидания!");
                         break;
                     }
-                    if (a == "Да") 
+                    if (a == "Да")
                     {
                         Console.WriteLine("Введите элементы матрицы: ");
                         for (int i = 0; i < M; i++) // ввод по строкам
@@ -40,11 +41,22 @@ namespace pr10
                             for (int j = 0; j < N; j++) // ввод по столбцам
                             {
                                 Console.Write("Элемент [" + i + "," + j + "]: ");
-                                Mas[i, j] = Convert.ToInt32(Console.ReadLine());
-
-                                if (Mas[i, j] != 0)
+                                try
                                 {
-                                    product *= Mas[i, j];
+                                    Mas[i, j] = Convert.ToInt32(Console.ReadLine());
+
+                                    if (Mas[i, j] != 0)
+                                    {
+                                        product *= Mas[i, j];
+                                        NotEmpty = true;
+                                    }
+                                }
+                                catch (FormatException fe)
+                                {
+                                    Console.ForegroundColor = ConsoleColor.Red;
+                                    Console.WriteLine($"\nОшибка ввода. Введите целое число. \n" + fe.Message);
+                                    Console.ForegroundColor = ConsoleColor.White;
+                                    j--;
                                 }
                             }
                             Console.WriteLine();
@@ -56,44 +68,46 @@ namespace pr10
                         {
                             for (int j = 0; j < N; j++)
                             {
-                                Mas[i, j] = rnd.Next(-100, 101); 
+                                Mas[i, j] = rnd.Next(-100, 101);
                                 Console.ForegroundColor = ConsoleColor.Green;
                                 Console.Write(Mas[i, j] + "\t");
-
+                                Console.ForegroundColor = ConsoleColor.White;
                                 if (Mas[i, j] != 0)
                                 {
                                     product *= Mas[i, j];
+                                    NotEmpty = true;
                                 }
                             }
                             Console.WriteLine();
                         }
                     }
-                    else // иначе
+                    else
                     {
                         Console.ForegroundColor = ConsoleColor.Yellow;
                         Console.WriteLine("Введите Да или Нет! Либо Выход. (Ввод ответа требуется с большой буквы)");
                         Console.ForegroundColor = ConsoleColor.White;
                         continue;
                     }
-                    Console.ForegroundColor = ConsoleColor.DarkGreen;
-                    Console.WriteLine("Произведение ненулевых элементов: " + product);
-                    Console.ForegroundColor = ConsoleColor.White;
+                    if (NotEmpty)
+                    {
+                        Console.ForegroundColor = ConsoleColor.DarkGreen;
+                        Console.WriteLine("Произведение ненулевых элементов: " + product);
+                        Console.ForegroundColor = ConsoleColor.White;
+                    }
                 }
                 catch (FormatException fe)
                 {
                     Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine($"\nОшибка ввода \n" + fe.Message); 
+                    Console.WriteLine($"\nОшибка ввода \n" + fe.Message);
                     Console.ForegroundColor = ConsoleColor.White;
                 }
-                catch (Exception e) 
+                catch (Exception e)
                 {
                     Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine($"\nОшибка ввода \n" + e.Message); 
+                    Console.WriteLine($"\nОшибка ввода \n" + e.Message);
                     Console.ForegroundColor = ConsoleColor.White;
                 }
             }
         }
     }
 }
-// не выводить матрицу если она равна нулю
-// добавить обработчик ошибок отдельно для матрицы
